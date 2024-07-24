ef745b17187b9773c9ee846c75afc13e3b83f858
omar.seba <omar.seba.pro@gmail.com>
2024-07-24 23:21:38 +0200

    my portfolio

---
+++ src/app/home/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useState } from "react";
+
+
+
+export default function Home() {
+  const [hoverId, setHoverId] = useState(null);
+
+  const treeData = {
+    id: 'root',
+    name: 'Root',
+    children: [
+      {
+        id: 'child1',
+        name: 'Child 1',
+        children: [
+          { id: 'grandchild1.1', name: 'Grandchild 1.1' },
+          { id: 'grandchild1.2', name: 'Grandchild 1.2' }
+        ]
+      },
+      {
+        id: 'child2',
+        name: 'Child 2',
+        children: [
+          { id: 'grandchild2.1', name: 'Grandchild 2.1' },
+          { id: 'grandchild2.2', name: 'Grandchild 2.2' }
+        ]
+      }
+    ]
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>home page</div>
+    </main>
+  );
+}
