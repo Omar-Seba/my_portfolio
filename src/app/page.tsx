b6d35d0a65aaa781e627d8a283611beadc5cda0a
omar.seba <omar.seba.pro@gmail.com>
2024-07-28 23:00:56 +0200

    logo for the navbar done

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from "react";
+import { useState } from "react";
+import Home from "./home/page";
+
+
+
+export default function App() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Home />
+    </main>
+  );
+}
