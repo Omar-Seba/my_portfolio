be74f52908849816b794dc6c24b83b51a09a7bcc
omar.seba <omar.seba.pro@gmail.com>
2024-09-01 22:50:48 +0200

    developer view / photoprapher view

---
+++ src/app/developpement/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from "react";
+import { useState } from "react";
+
+
+
+export default function App() {
+
+
+  return (
+    <main className="flex flex-col justify-between items-center p-24 min-h-screen">
+      cew
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
 'use client'
 import React from "react";
-import { useState } from "react";
-import Home from "./home/page";
-
-
+import { motion } from 'framer-motion';
 
 export default function App() {
-
-
   return (
-    <main className="flex flex-col justify-between items-center p-24 min-h-screen">
-      <Home />
+    <main className="grid grid-cols-2 h-screen">
+      <motion.div
+        className="relative flex justify-center items-center bg-opacity-50 bg-cover bg-center h-full"
+        initial={{
+          backgroundImage: "url('')",  // Initially no background
+          backgroundSize: "100%",
+          opacity: 1,  // Ensure the title is always visible
+          x: 0
+        }}
+        whileHover={{
+          backgroundImage: "url('/code.jpg')",
+          backgroundSize: "105%",
+          transition: { duration: 0.5, ease: "easeInOut" }
+        }}
+      >
+        <div className="flex justify-center items-center bg-black bg-opacity-0 hover:bg-opacity-70 w-full h-full">
+          <span className="font-mono text-white text-xl">Developer</span>
+
+        </div>
+      </motion.div>
+      <motion.div
+        className="relative flex justify-center items-center bg-opacity-50 bg-cover bg-center h-full"
+        initial={{
+          backgroundImage: "url('')",  // Initially no background
+          backgroundSize: "100%",
+          opacity: 1,  // Ensure the title is always visible
+          x: 0
+        }}
+        whileHover={{
+          backgroundImage: "url('/EffeilDark.JPG')",
+          backgroundSize: "105%",
+          transition: { duration: 0.5, ease: "easeInOut" }
+
+        }}
+      >
+        <div className="flex justify-center items-center bg-black bg-opacity-0 hover:bg-opacity-70 w-full h-full">
+          <span className="font-serif text-white text-xl italic">Photographer</span>
+
+        </div>
+      </motion.div>
     </main>
   );
 }
