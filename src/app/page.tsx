c9e2436a9341b3831205b6b293b13da531953f72
omar.seba <omar.seba.pro@gmail.com>
2024-09-13 20:14:31 +0200

    responsive landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,11 @@ export default function App() {
       className="relative w-full h-screen overflow-hidden group"
 
     >
-      <div className='relative grid grid-cols-2 w-full h-screen'>
+      <div className='relative grid grid-cols-1 lg:grid-cols-2 w-full h-screen'>
 
         <Link href="/development">
           <motion.div
-            className='flex bg-gradient-to-r from-fuchsia-900 via-purple-900 to-indigo-900 w-full h-full'
+            className='flex bg-gradient-to-r from-fuchsia-950 via-purple-950 to-indigo-950 w-full h-full'
           >
             <motion.div onMouseEnter={() => {
               setIsHovered(true)
@@ -42,9 +42,9 @@ export default function App() {
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat',
               }}
-              className="relative hover:grayscale-0 bg-cover bg-center m-2 hover:mb-[4rem] w-full duration-300 ease-in grayscale">
+              className="relative hover:grayscale-0 lg:block hidden bg-cover bg-center m-2 hover:mb-[4rem] w-full duration-300 ease-in grayscale">
               {isHovered && <span
-                className="-bottom-12 left-2 absolute font-mono text-xl md:text-4xl">
+                className="-bottom-12 left-2 absolute font-mono text-xl lg:text-4xl">
                 Developer
                 <span className='left-0 absolute blur'>
                   Developer
@@ -53,11 +53,23 @@ export default function App() {
               </span>}
 
             </motion.div>
+            {/* modible version */}
+            <motion.div className="relative flex justify-center items-center lg:hidden bg-cover bg-center m-2 w-full duration-300 ease-in">
+              <motion.span className="relative z-10 font-mono text-xl">
+                Developer
+                <span className='left-0 absolute blur'>
+                  Developer
+
+                </span>
+              </motion.span>
+
+              <div className="absolute inset-0 bg-[url('/code.jpg')] bg-cover bg-center opacity-25"></div>
+            </motion.div>
           </motion.div>
         </Link>
         <Link href="/photography">
           <motion.div
-            className='flex bg-gradient-to-r from-indigo-900 via-blue-900 to-sky-900 w-full h-full'
+            className='flex bg-gradient-to-r from-indigo-950 via-blue-950 to-sky-950 w-full h-full'
           >
             <motion.div onMouseEnter={() => {
               setIsHovered(true)
@@ -70,7 +82,7 @@ export default function App() {
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat',
               }}
-              className="relative hover:grayscale-0 bg-cover bg-center m-2 hover:mb-[4rem] w-full duration-300 ease-in grayscale">
+              className="relative hover:grayscale-0 lg:block hidden bg-cover bg-center m-2 hover:mb-[4rem] w-full duration-300 ease-in grayscale">
 
               {isHovered &&
                 <motion.span
@@ -80,12 +92,21 @@ export default function App() {
                       duration: 0.5
                     }
                   }}
-                  className="-bottom-12 left-2 absolute font-serif text-xl md:text-4xl italic">
+                  className="-bottom-12 left-2 lg:absolute flex font-serif text-xl lg:text-4xl italic">
                   Photographer
-                  <span className='left-0 absolute blur'>
+                  <span className='left-0 lg:absolute flex blur'>
                     Photographer
                   </span>
                 </motion.span>}
+            </motion.div>
+            {/* modible version */}
+            <motion.div className="relative flex justify-center items-center lg:hidden bg-cover bg-center m-2 w-full duration-300 ease-in">
+              <motion.span className="relative z-10 font-serif text-xl italic">
+                Photographer
+                <span className="left-0 absolute blur">Photographer</span>
+              </motion.span>
+
+              <div className="absolute inset-0 bg-[url('/alpes.jpg')] bg-cover bg-center opacity-25"></div>
             </motion.div>
           </motion.div>
           {/* </motion.div> */}
